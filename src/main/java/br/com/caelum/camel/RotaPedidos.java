7b30116b22097080a29767a1c0c3bdf2406a5b74
wellington.siqueira <wellingtonex@gmail.com>
2017-01-12 11:02:23 -0300

    filtranto resultados por formato do livro com o metodo split.xpath e filter.xpath

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -15,6 +15,9 @@ public class RotaPedidos {
 			public void configure() throws Exception {
 
 				from("file:pedidos?delay=5s&noop=true").
+					split().xpath("/pedido/itens/item").
+					log("${body}").
+					filter().xpath("/item/formato[text()='EBOOK']").
 					log("${id}").
 					marshal().xmljson().
 					log("${body}").
