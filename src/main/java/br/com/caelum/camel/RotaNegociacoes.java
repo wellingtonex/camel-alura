36ace9751af5819ad6fdb01cab5293b83803e990
wellington.siqueira <wellingtonex@gmail.com>
2017-01-12 23:38:18 -0300

    lendo de um serviço a partir de um time e transformando em uma lista de objetos utilizando o xstream

--- src/main/java/br/com/caelum/camel/RotaNegociacoes.java
+++ src/main/java/br/com/caelum/camel/RotaNegociacoes.java
@@ -1,8 +1,8 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
-import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.xstream.XStreamDataFormat;
 import org.apache.camel.impl.DefaultCamelContext;
 
 import com.thoughtworks.xstream.XStream;
@@ -10,6 +10,7 @@ import com.thoughtworks.xstream.XStream;
 public class RotaNegociacoes {
 
 	public static void main(String[] args) throws Exception {
+		
 
 		CamelContext context = new DefaultCamelContext();
 		context.addRoutes(new RouteBuilder() {
@@ -18,13 +19,14 @@ public class RotaNegociacoes {
 			public void configure() throws Exception {
 				final XStream xstream = new XStream();
 				xstream.alias("negociacao", Negociacao.class);
-
-				from("timer://negociacoes?fixedRate=true&delay=1s&period=360s")
-						.to("http4://argentumws.caelum.com.br/negociacoes")
-						.convertBodyTo(String.class).log("${body}")
-						.setHeader(Exchange.FILE_NAME,
-								constant("negociacoes.xml"))
-						.to("file:saida");
+				
+			    from("timer://negociacoes?fixedRate=true&delay=3s&period=360s")
+			      .to("http4://argentumws.caelum.com.br/negociacoes")
+			      .convertBodyTo(String.class)
+			      .unmarshal(new XStreamDataFormat(xstream))
+			      .split(body())
+			      .log("${body}")
+			    .end(); //só deixa explícito que é o fim da rota
 			}
 		});
 
