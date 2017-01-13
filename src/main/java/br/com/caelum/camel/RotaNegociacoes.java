27f5cd2acca8ef1d45e2817aae1169b0a836dfa1
wellington.siqueira <wellingtonex@gmail.com>
2017-01-12 23:55:22 -0300

    lendo de um serviço a partir de um time e transformando em uma lista de objetos utilizando o xstream e salvando em um banco de dados (muito loco, tendi nada)

--- src/main/java/br/com/caelum/camel/RotaNegociacoes.java
+++ src/main/java/br/com/caelum/camel/RotaNegociacoes.java
@@ -1,18 +1,26 @@
 package br.com.caelum.camel;
 
+import java.text.SimpleDateFormat;
+
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.xstream.XStreamDataFormat;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.SimpleRegistry;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource;
 import com.thoughtworks.xstream.XStream;
 
 public class RotaNegociacoes {
 
 	public static void main(String[] args) throws Exception {
 		
+		SimpleRegistry registro = new SimpleRegistry();
+		registro.put("mysql", criaDataSource());
 
-		CamelContext context = new DefaultCamelContext();
+		CamelContext context = new DefaultCamelContext(registro);
 		context.addRoutes(new RouteBuilder() {
 
 			@Override
@@ -20,18 +28,40 @@ public class RotaNegociacoes {
 				final XStream xstream = new XStream();
 				xstream.alias("negociacao", Negociacao.class);
 				
-			    from("timer://negociacoes?fixedRate=true&delay=3s&period=360s")
-			      .to("http4://argentumws.caelum.com.br/negociacoes")
-			      .convertBodyTo(String.class)
-			      .unmarshal(new XStreamDataFormat(xstream))
-			      .split(body())
-			      .log("${body}")
-			    .end(); //só deixa explícito que é o fim da rota
+				from("timer://negociacoes?fixedRate=true&delay=3s&period=360s").
+			    to("http4://argentumws.caelum.com.br/negociacoes").
+			      convertBodyTo(String.class).
+			      unmarshal(new XStreamDataFormat(xstream)).
+			      split(body()).
+			      process(new Processor() {
+			        @Override
+			        public void process(Exchange exchange) throws Exception {
+			            Negociacao negociacao = exchange.getIn().getBody(Negociacao.class);
+			            exchange.setProperty("preco", negociacao.getPreco());
+			            exchange.setProperty("quantidade", negociacao.getQuantidade());
+			            String data = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss").format(negociacao.getData().getTime());
+			            exchange.setProperty("data", data);
+			        }
+			      }).
+			      setBody(simple("insert into negociacao(preco, quantidade, data) values (${property.preco}, ${property.quantidade}, '${property.data}')")).
+			      log("${body}").
+			      delay(1000).
+			to("jdbc:mysql");
 			}
 		});
 
 		context.start();
 		Thread.sleep(20000);
 		context.stop();
+	}
+	
+	private static MysqlConnectionPoolDataSource criaDataSource() {
+	    MysqlConnectionPoolDataSource mysqlDs = new MysqlConnectionPoolDataSource();
+	    mysqlDs.setDatabaseName("camel");
+	    mysqlDs.setServerName("localhost");
+	    mysqlDs.setPort(3306);
+	    mysqlDs.setUser("root");
+	    mysqlDs.setPassword("root");
+	    return mysqlDs;
 	}
 }
