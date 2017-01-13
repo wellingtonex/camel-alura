a0af36f6477c2f65f6ce45ae1e3e85fce445d02d
wellington.siqueira <wellingtonex@gmail.com>
2017-01-12 23:29:16 -0300

    lendo de um serviço a partir de um time e escrevendo em um arquivo

---
+++ src/main/java/br/com/caelum/camel/RotaNegociacoes.java
@@ -0,0 +1,35 @@
+package br.com.caelum.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+import com.thoughtworks.xstream.XStream;
+
+public class RotaNegociacoes {
+
+	public static void main(String[] args) throws Exception {
+
+		CamelContext context = new DefaultCamelContext();
+		context.addRoutes(new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+				final XStream xstream = new XStream();
+				xstream.alias("negociacao", Negociacao.class);
+
+				from("timer://negociacoes?fixedRate=true&delay=1s&period=360s")
+						.to("http4://argentumws.caelum.com.br/negociacoes")
+						.convertBodyTo(String.class).log("${body}")
+						.setHeader(Exchange.FILE_NAME,
+								constant("negociacoes.xml"))
+						.to("file:saida");
+			}
+		});
+
+		context.start();
+		Thread.sleep(20000);
+		context.stop();
+	}
+}
